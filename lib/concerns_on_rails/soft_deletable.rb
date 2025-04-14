fd77125fcfe4a72c91a8656bef1c50c893e725b4
Ethan <doctorit@gmail.com>
2025-04-14 23:04:25 +0700

    Add SoftDeletable module

---
+++ lib/concerns_on_rails/soft_deletable.rb
@@ -0,0 +1,15 @@
+require "active_support/concern"
+
+module ConcernsOnRails
+  module SoftDeletable
+    extend ActiveSupport::Concern
+
+    included do
+
+    end
+
+    class_methods do
+      
+    end
+  end
+end
